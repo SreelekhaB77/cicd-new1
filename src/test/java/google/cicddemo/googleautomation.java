191a63429af7ad7ccf71462e9d1ee5794d8b7745
Veeramany.V <Veeramany.V@LAPTOP-47NA6DSS>
2024-02-10 12:21:04 +0530

    demo commit

---
+++ src/test/java/google/cicddemo/googleautomation.java
@@ -0,0 +1,36 @@
+package google.cicddemo;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleautomation {
+	public static RemoteWebDriver driver;
+
+@SuppressWarnings("deprecation")
+@Test
+public static void launch() throws InterruptedException
+{
+
+	System.out.println("hi");		
+	
+    WebDriverManager.firefoxdriver().setup();
+    driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+    driver.get("https://www.google.com/");
+    System.out.println(driver.getTitle());
+    System.out.println("hi");
+}	
+}
